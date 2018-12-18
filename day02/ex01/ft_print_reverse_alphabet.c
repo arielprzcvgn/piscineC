f6a7eaf0ae6a49fc1ab58eab52a086bd1d82d200
Ariel Perez <45876878+arielprzcvgn@users.noreply.github.com>
2018-12-18 22:11:01 +0100

    day02 upload

---
+++ day02/ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ariperez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/05 08:17:16 by ariperez          #+#    #+#             */
+/*   Updated: 2018/07/08 13:55:51 by ariperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		ft_putchar(letter);
+		letter--;
+	}
+}
---
+++ day02/ex06/ft_putnbr.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ariperez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/05 17:23:20 by ariperez          #+#    #+#             */
+/*   Updated: 2018/07/08 13:59:50 by ariperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	negative_extreme(void)
+{
+	ft_putchar('-');
+	ft_putchar('2');
+	ft_putchar('1');
+	ft_putchar('4');
+	ft_putchar('7');
+	ft_putchar('4');
+	ft_putchar('8');
+	ft_putchar('3');
+	ft_putchar('6');
+	ft_putchar('4');
+	ft_putchar('8');
+}
+
+int		pow10(int nb)
+{
+	long i;
+
+	i = 1;
+	while ((nb / (i * 10)) != 0)
+		i = i * 10;
+	return (i);
+}
+
+void	ft_putnbr(int nb)
+{
+	int i;
+
+	if (nb == -2147483648)
+		negative_extreme();
+	else
+	{
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			nb = -nb;
+		}
+		i = pow10(nb);
+		while (nb > 9)
+		{
+			ft_putchar((nb / i) + '0');
+			nb = nb - (nb / i) * i;
+			i = i / 10;
+		}
+		ft_putchar(nb + '0');
+	}
+}
