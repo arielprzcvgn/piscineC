b7f4dd7ad6e5a70e01cfff81f9957c2eacbaec84
Ariel Perez <45876878+arielprzcvgn@users.noreply.github.com>
2018-12-19 21:00:00 +0100

    day05 upload

---
+++ day05/ex18/ft_strlcat.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ariperez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/11 14:30:59 by ariperez          #+#    #+#             */
+/*   Updated: 2018/07/16 12:39:50 by ariperez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int		i;
+	unsigned int		j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0' && i < size)
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (i + j);
+}
